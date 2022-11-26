7bd6f45ad101dae01e5ae5c138cc380ec7a0e25d
mifan002 <fanmingcheng516@gmail.com>
2022-11-26 23:52:30 +0100

    let_it_snow.py can generate snowflakes with random colors now

--- let_it_snow.py
+++ let_it_snow.py
@@ -11,10 +11,9 @@ def main(speed=0, bg_color="grey"):
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
  
     """TODO: define different colors here"""
-
+    random_color = np.random.rand(10, 3)
 
     for _ in range(10):
         # define some params
@@ -22,7 +21,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
-
+        myTurtle.pencolor(random_color[_])
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
